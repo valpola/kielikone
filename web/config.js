ee9424dc8c69f70e2716afe225ee7fc4197223b9
Harri Valpola <harri@system2ai.com>
2026-02-21 23:14:08 +0200

    Enable Google Sheets logging

--- web/config.js
+++ web/config.js
@@ -1,4 +1,5 @@
 const APP_CONFIG = {
-  resultsEndpoint: "",
-  resultsEnabled: false,
+  resultsEndpoint:
+    "https://script.google.com/macros/s/AKfycbx6v4L6Bsp6Xw41rCgkHj9BtjpcYt9RfiEvs7O7HqZR-yYAcZ55m-Y2wWHPkk0kLFkv/exec",
+  resultsEnabled: true,
 };
